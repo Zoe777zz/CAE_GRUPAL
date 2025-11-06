1e39f55849527c84c941104cabc0ed688d75b51a
Zoe777zz <soledadburicamacho2@gmail.com>
2025-11-06 07:38:17 -0500

    Update Reversible.java

--- Grupal_Cae/src/c_logica/Reversible.java
+++ Grupal_Cae/src/c_logica/Reversible.java
@@ -2,10 +2,6 @@ package c_logica;
 
 import d_historial.Accion;
 
-/**
- * interfaz que define el contrato de las operaciones que se pueden deshacer y rehacer.
- * implementa el principio de inversion de dependencias (DIP) de SOLID.
- */
 public interface Reversible {
 
     /**
